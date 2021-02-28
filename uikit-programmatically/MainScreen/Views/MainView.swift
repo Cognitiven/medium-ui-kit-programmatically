a37c9236c35108cf5ad86a0926bb0b0b6d7ea979
Martin Lachev <martin.lachev@flowscapesolutions.com>
2021-02-28 23:18:29 +0200

    Adding constraints

--- uikit-programmatically/MainScreen/Views/MainView.swift
+++ uikit-programmatically/MainScreen/Views/MainView.swift
@@ -18,34 +18,71 @@ class MainView: View {
     }
 
     private func setupUI() {
-        backgroundColor = .red
+        backgroundColor = .white
     }
 
     private func setupViews() {
         addSubview(imageView)
+        addSubview(textView)
     }
 
     private func setupConstraints() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
 
-        sharedConstraints.append(contentsOf: addImageViewSharedConstraints())
 
+        sharedConstraints.append(contentsOf: addSharedConstraints())
+        regularConstraints.append(contentsOf: addRegularConstraints())
+        compactConstraints.append(contentsOf: addCompactConstraints())
     }
 
     // MARK: Constraints
-    private func addImageViewSharedConstraints() -> [NSLayoutConstraint] {
+
+    private func addSharedConstraints() -> [NSLayoutConstraint] {
         return [
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            imageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
-            imageView.widthAnchor.constraint(equalToConstant: 49),
-            imageView.heightAnchor.constraint(equalToConstant: 49)
+            textView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0)
+        ]
+    }
+
+    private func addCompactConstraints() -> [NSLayoutConstraint] {
+        return [
+            //imageView
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            //textView
+            textView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            textView.widthAnchor.constraint(equalToConstant: 350),
+        ]
+    }
+
+    private func addRegularConstraints() -> [NSLayoutConstraint] {
+        return [
+            //imageView
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 200),
+            imageView.widthAnchor.constraint(equalToConstant: 400),
+            imageView.heightAnchor.constraint(equalToConstant: 400),
+            //textView
+            textView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            textView.widthAnchor.constraint(equalToConstant: 700),
         ]
     }
 
     // MARK: Views
-    let imageView: UIView = {
-        let imageView = UIView()
-        imageView.backgroundColor = .white
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "su")
         return imageView
+    }()
+
+    let textView: UILabel = {
+        let textView = UILabel()
+        textView.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        textView.textColor = .black
+        print(textView.calculateMaxLines())
+        textView.numberOfLines = 12
+        textView.lineBreakMode = .byWordWrapping
+        return textView
     }()
 }
