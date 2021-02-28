b17a9941fba29a831b6154914d51ca5b9adbbb81
Martin Lachev <martinlachev@gmail.com>
2021-02-28 15:33:32 +0200

    Initial Commit

---
+++ uikit-programmaticaaly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-programmaticaaly
+//
+//  Created by Martin Lachev on 28.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
